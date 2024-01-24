961a56edafc867f32c318182c392ad2a411e8ec7
Ihor Prochnickiy <2prochigor@gmail.com>
2024-01-24 09:01:03 +0200

    add dynamic detection of phone udid

--- utils/android_utils.py
+++ utils/android_utils.py
@@ -1,3 +1,6 @@
+import subprocess
+
+
 def android_get_desired_capabilities():
     return {
         'autoGrantPermissions': True,
@@ -9,7 +12,19 @@ def android_get_desired_capabilities():
         'resetKeyboard': True,
         'systemPort': 8301,
         'takesScreenshot': True,
-        'udid': 'emulator-5554',
+        'udid': get_udid(),
         'appPackage': 'com.ajaxsystems',
         'appActivity': 'com.ajaxsystems.ui.activity.LauncherActivity'
-}
+    }
+
+
+def get_udid():
+    adb_devices_output = subprocess.check_output(["adb", "devices"], encoding="utf-8")
+    lines = adb_devices_output.strip().split('\n')
+
+    if len(lines) > 1:
+        udid = lines[1].split('\t')[0]
+        return udid
+    else:
+        print("No devices found.")
+        return None
